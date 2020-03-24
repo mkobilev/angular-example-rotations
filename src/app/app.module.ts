6781ebe39c446727c41203e2054f2c51c40b7983
Max Kobilev <mkobilev@gmail.com>
2020-03-24 19:05:30 +0300

    feat: add ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 // import { RotationListComponent } from './rotation-list/rotation-list.component';
 // import { RotationItemComponent } from './rotation-item/rotation-item.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { registerLocaleData } from '@angular/common';
+import localeEn from '@angular/common/locales/en';
+
+registerLocaleData(localeEn, 'en-EN');
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    // RotationListComponent,
-    // RotationItemComponent
-  ],
-  imports: [
-    BrowserModule,
-    CoreModule,
-    AppRoutingModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        // RotationListComponent,
+        // RotationItemComponent
+    ],
+    imports: [
+        NgbModule,
+        BrowserModule,
+        CoreModule,
+        AppRoutingModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
